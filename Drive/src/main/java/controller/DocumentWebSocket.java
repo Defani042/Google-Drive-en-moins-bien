5d3c283097153bcd230dca6a64a701f69e0ebf29
Frillbug00 <dorian.breuil9@orange.fr>
2026-05-14 13:21:36 +0200

    feat: Ajout DocumentWebSocket

---
+++ Drive/src/main/java/controller/DocumentWebSocket.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+
+@ServerEndpoint("/DocumentWebSocket")
+public class DocumentWebSocket {
+	static List<Session> mesSessions = new ArrayList<>();
+	
+	@OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Utilisateur connecté : " + session.getId());
+        mesSessions.add(session);
+        try {
+			session.getBasicRemote().sendText(
+					"Utilisateur connecté" // Texte à l'envoi
+			);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("Utilisateur déconnecté : " + session.getId());
+		mesSessions.remove(session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("Reçu : " + message);
+        //sendUpdateToAll(session);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable t) {
+        t.printStackTrace();
+    }
+}
+
